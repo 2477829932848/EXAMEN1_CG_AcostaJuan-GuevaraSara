c1f0370e6c9122406e8d9ee29556dc09c6a483a0
2477829932848 <strewlivkcp12@gmail.com>
2025-10-01 15:46:18 -0500

    details

--- ExamenJAySG/src/app/details/details.component.ts
+++ ExamenJAySG/src/app/details/details.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-details',
-  imports: [],
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  styleUrls: ['./details.component.css'],
+  standalone: true,
+  imports: [ToastModule, ButtonModule],
+  providers: [MessageService]
 })
 export class DetailsComponent {
+  constructor(private messageService: MessageService) {}
 
+  showSuccess() {
+    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Operación realizada con éxito' });
+  }
+
+  showInfo() {
+    this.messageService.add({ severity: 'info', summary: 'Info', detail: 'Información adicional' });
+  }
+
+  showWarn() {
+    this.messageService.add({ severity: 'warn', summary: 'Warning', detail: 'Advertencia detectada' });
+  }
+
+  showError() {
+    this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Ha ocurrido un error' });
+  }
 }
