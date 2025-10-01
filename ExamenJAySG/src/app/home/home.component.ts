1a55be9a0355eeaf3329e912e6320cdfb7620706
2477829932848 <strewlivkcp12@gmail.com>
2025-10-01 14:50:54 -0500

    estilos

--- ExamenJAySG/src/app/home/home.component.ts
+++ ExamenJAySG/src/app/home/home.component.ts
@@ -1,11 +1,49 @@
+
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  // Datos de menú
+  items = [
+    {
+      label: 'Inicio', routerLink: '/home'
+    },
+    {
+      label: 'Características'
+    },
+    {
+      label: 'Proyectos',
+      items: [
+        { label: 'Software a la medida' },
+        { label: 'Desarrollo móvil' },
+        { label: 'Soporte a aplicaciones' }
+      ]
+    },
+    {
+      label: 'Contacto'
+    }
+  ];
 
+  // Datos de cards
+  services = [
+    {
+      title: 'Desarrollo de software a la medida',
+      img: 'assets/software.png',
+      desc: 'Creamos soluciones adaptadas a tus necesidades.'
+    },
+    {
+      title: 'Desarrollo móvil',
+      img: 'assets/mobile.png',
+      desc: 'Aplicaciones móviles para iOS y Android.'
+    },
+    {
+      title: 'Soporte a aplicaciones',
+      img: 'assets/support.png',
+      desc: 'Mantenimiento y optimización de sistemas existentes.'
+    }
+  ];
 }
