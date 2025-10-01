322b20f9adcf3b21240fd84ec07c68111871d388
2477829932848 <strewlivkcp12@gmail.com>
2025-10-01 18:01:14 -0500

    "Final"

--- ExamenJAySG/src/app/home/home.component.ts
+++ ExamenJAySG/src/app/home/home.component.ts
@@ -1,17 +1,26 @@
 
 import { Component } from '@angular/core';
-import { AccordionBasicDemo } from './componentes_home/accordion/accordion.component';
 import { ButtonBasicDemo } from './componentes_home/button/button.component';
 import { CardAdvancedDemo } from './componentes_home/cards/cards.component';
 import { ImageTemplateDemo } from './componentes_home/image/image.component';
+import { AccordionBasicDemo } from './componentes_home/accordion/accordion.component';
 import { InputTextBasicDemo } from './componentes_home/input-text/input-text.component';
 import { MenubarTemplateDemo } from './componentes_home/menu-bar/menu-bar.component';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
-})
+  styleUrls: ['./home.component.css'],
+  imports : [
+    ButtonBasicDemo,
+    CardAdvancedDemo,
+    ImageTemplateDemo,
+    AccordionBasicDemo,
+    InputTextBasicDemo,
+    MenubarTemplateDemo
+  ]
+});
 export class HomeComponent {
   // Datos de menú
   items = [
