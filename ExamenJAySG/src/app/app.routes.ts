e1c65b56f455667fc6530c3680ab0b7d7d8682a7
2477829932848 <strewlivkcp12@gmail.com>
2025-10-01 14:37:33 -0500

    "Rutas /home y /details"

--- ExamenJAySG/src/app/app.routes.ts
+++ ExamenJAySG/src/app/app.routes.ts
@@ -1,5 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
 
 export const routes: Routes = [
-    
+    {
+        path: 'home',
+        component : HomeComponent
+    },
+    {
+        path: 'details',
+        component : DetailsComponent
+    }
 ];
